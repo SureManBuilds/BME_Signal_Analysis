426ecff160f2840ac685dc5dc929ef3e176d98e8
SureMan <79489773+SureManBuilds@users.noreply.github.com>
2021-02-22 22:51:45 -0800

    Update Select_Specific_Files.m

--- Select_Specific_Files.m
+++ Select_Specific_Files.m
@@ -1,16 +1,15 @@
 function [Selection_Content, File_Names] = Select_Specific_Files(File_Type_In, Selection_Type)
-%This opens file browser and asks you to select files.
-[File_Names,Dir_Path] = uigetfile(File_Type_In,...
-   char(strcat('Select One or More',Selection_Type',' Files')), ...
-   'MultiSelect', 'on');
+[File_Names,Dir_Path] = uigetfile(File_Type_In, char(strcat('Select One or More',Selection_Type',' Files')), 'MultiSelect', 'on');
+
 if isequal(File_Names,0)
-  % disp('User selected Cancel');
   Selection_Content = [];
   File_Names = [];
 else
     Selection_Content = cellstr(fullfile(Dir_Path,File_Names));
     File_Names = cellstr(File_Names);
 end
+
+
 end
 
 
